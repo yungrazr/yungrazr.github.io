8befbcbb60ad7546a126554966f7f5b3c8c586fd
Daniel Vegera <daniel.vegera@gmail.com>
2022-03-12 15:09:32 +0000

    Update

---
+++ _next/static/znB445XWzcGS3H6oWlF-L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/640-ff11cb7e42c8f89e.js","static/chunks/pages/index-8322ce23dde81719.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
